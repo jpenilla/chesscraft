1644d19ed720d517d1694ef01052a068bb638e51
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-02 16:16:43 -0700

    bump gremlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
-pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    maven("https://repo.jpenilla.xyz/snapshots/")
-  }
-}
-
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
