b5bbd87905d39f22892ca1d3b5ef11eefc0717a5
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-02-12 13:29:58 -0700

    cloud-minecraft beta 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
   }
   implementation("xyz.niflheim:stockfish-java:4.0.0-SNAPSHOT")
   implementation(platform("org.incendo:cloud-bom:2.0.0-beta.2"))
-  implementation(platform("org.incendo:cloud-minecraft-bom:2.0.0-SNAPSHOT"))
+  implementation(platform("org.incendo:cloud-minecraft-bom:2.0.0-beta.3"))
   implementation("org.incendo:cloud-paper")
   implementation("org.incendo:cloud-translations-core:1.0.0-SNAPSHOT")
   implementation("org.incendo:cloud-translations-bukkit:1.0.0-SNAPSHOT")
